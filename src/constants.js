92e6d1a44ece9839cb30c45470806f7baa9d22ff
ewdwins <saleh.tarkashvand@gmail.com>
2024-08-19 19:22:05 +0330

    final changing

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  backdor: `Are you leaving my room ?`,
+  degree: `Hello everyone!
+    </br>
+    My name is Saleh Torkashvand, and I would like to invite you to visit my LinkedIn page. On my page, you can learn more about my projects, skills, and experiences. If you are interested in collaboration or exchanging ideas, I would be happy to connect with you.
+    </br>
+    <a href="https://www.linkedin.com/in/saleh-tarkshvand-67049026a/" target="_blank">my page ...</a>"
+    </br>
+    Looking forward to connecting with you!`,
+  table: `When I sit on my couch, I immerse myself in its comfort and tranquility, and I think about my dreams and goals.</br>
+   In these moments, my mind fills with ideas and plans that I want to achieve.</br>
+    My couch is a place where I can comfortably contemplate my future and get motivated to get closer to my dreams. Every time I sit on this couch, I feel like I am one step closer to realizing my dreams, and I move towards my goals with more energy and motivation. </br> 
+    Sometimes, I even work on my laptop until morning, and during these moments, my couch is my best companion`,
+
+  pc: `My old computer, you have always been more than just a machine to me. Six years ago, when I wrote my first lines of code on you, I never imagined where this journey would lead. You were with me as I wrote my first programs, discovering new worlds with each line of code.</br>
+
+With you, I learned how to solve problems, how to learn from failures, and how to achieve great successes. You gave me the confidence to reach for the best, and now, you sit in the corner of my room as a cherished memento. </br>
+
+Every time I look at you, I remember how I started from nothing and got to where I am today. You symbolize my efforts and perseverance, and you will always have a special place in my heart. Thank you for being with me and helping me reach my dreams.`,
+
+  tv: `Since childhood, I have been passionate about video games and always played on the TV. I was a dedicated gamer, and gaming was not just a hobby but a great passion for me. I always dreamed of creating my own game one day.</br>
+
+After much effort, I finally succeeded in creating my first game. The game I made was Flappy Bird. This experience was very exciting and educational for me. If you would like, you can try my game. Here is the link: Game Link.</br>
+
+I hope you enjoy the game and share your feedback with me! </br> </br>
+   <a href="http://flappy-bird-pied.vercel.app" target="_blank">flappy Bird Game</a>"`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/salehTarkashvand" target="_blank">Check it out?</a>
+  Contact me at saleh.tarkashvand@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `Reading books has always been a passion of mine, especially those that contribute to my personal and professional growth.</br> One of the most influential books I’ve read is “Clean Code” by Robert C. Martin. This book has significantly improved my coding practices and deepened my understanding of writing clean, efficient, and maintainable code. </br> It’s a must-read for any developer aiming to enhance their skills and produce high-quality software.
+
+In addition to reading, I have a strong desire to write my own book on success. I believe that sharing my journey, experiences, and the lessons I’ve learned can inspire others to pursue their goals and achieve their dreams. Writing this book would be a way to give back to the community and help others on their path to success.`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
